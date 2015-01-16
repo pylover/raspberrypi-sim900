3f27ef3e849f601190ccd4f789b4f0b50c526b08
pylover <vahid.mardani@gmail.com>
2015-01-16 06:44:37 +0330

    Planning

---
+++ raspberrypi/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vahid'
+
+__import__('pkg_resources').declare_namespace(__name__)
---
+++ raspberrypi/sim900/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vahid'
+
+__version__ = '0.1.0-prealpha'
